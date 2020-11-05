6e48286e664b9b819a6a746c2017f5956bb46044
newtampacperson <cory.person@patelhs.org>
2020-11-05 18:05:24 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+    int seats[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+    String[] names = new String[15];
+
+    Scanner sc = new Scanner(System.in);
+
+    for (int i = 0; i < names.length; i++)
+    {
+      System.out.print("\nIdentify yourself:\n\n");
+      names[i] = sc.nextLine();
+    }
+    int i = 0;
+    while(i < names.length && i < seats.length)
+    {
+      System.out.println(names[i] + " got " + seats[i] + " problems correct!");
+      i++;
+    }
+  }
+}
